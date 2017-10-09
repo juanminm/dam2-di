0e030c57334155d52466cd8434c9d42ec68efb38
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-10-10 01:53:48 +0200

    Refactor sucesion method and add comments

--- Listas2/Programa4.py
+++ Listas2/Programa4.py
@@ -1,16 +1,18 @@
 def sucesion(a, b, u, n):
     u = a*u+b
     n -= 1
-
+    
+    # Creamos una lista que contenga el valor de u que corresponda a la posición
+    # correcta dentro de la sucesión.
     lista_sucesion = [u]
-
-
-    if (n == 1):
-        return lista_sucesion
-    else:
+    
+    # Si n != 1 (1 es último termino de la sucesión) extenderá la lista
+    # recurriendo la función hasta que n sea 1. Tras finalizar la recursión,
+    # lista_sucesion será una lista con 'n' terminos.
+    if (n != 1):
         lista_sucesion.extend(sucesion(a, b, u, n))
-
-        return lista_sucesion
+    
+    return lista_sucesion
 
 a = int(input("Dígame el valor de a: "))
 b = int(input("Dígame el valor de b: "))
--- Listas2/Programa5.py
+++ Listas2/Programa5.py
@@ -3,16 +3,20 @@ def sucesion(u, n):
         u = u / 2
     else:
         u = 3 * u + 1
-
+    
+    # Creamos una lista que contenga el valor de u que corresponda a la posición
+    # correcta dentro de la sucesión.
+    # correcta dentro de la sucesión.
     lista_sucesion = [u]
     n -= 1
-
-    if (n == 1):
-        return lista_sucesion
-    else:
+    
+    # Si n != 1 (1 es último termino de la sucesión) extenderá la lista
+    # recurriendo la función hasta que n sea 1. Tras finalizar la recursión,
+    # lista_sucesion será una lista con 'n' terminos.
+    if (n != 1):
         lista_sucesion.extend(sucesion(u, n))
-
-        return lista_sucesion
+    
+    return lista_sucesion
 
 u0 = int(input("Dígame el valor de U(0): "))
 n = int(input("Digame cuántos términos quieres: "))
