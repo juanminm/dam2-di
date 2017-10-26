80981022fd9c9fc86e84b1c63ed3adfa48ef7fa0
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-10-26 12:24:29 +0200

    Reformat code

--- PPO/Paises.py
+++ PPO/Paises.py
@@ -16,12 +16,22 @@ class Pais:
 espanya = Pais("España", 46770000, 504645)
 francia = Pais("Francia", 66030000, 640679)
 
-# En este bloque uso una sintaxis de if parecida a una operación ternaria de Java (ex: a > b ? a : b). En el primer caso
-# devolverá la cadena "" si espanya.mas_grande_que(francia) devuelve True; en caso contrario, la cadena devuelta es
-# "no ".
-print("{} {}es mas grande que {}".format(espanya.nombre,
-                                         "" if espanya.mas_grande_que(francia) else "no ",
-                                         francia.nombre))
+# En este bloque uso una sintaxis de if parecida a una operación ternaria de
+# Java (ex: a > b ? a : b). En este caso devolverá la cadena "" si
+# espanya.mas_grande_que(francia) devuelve True; en caso contrario, la cadena
+# devuelta es "no ".
+print("{} {}es mas grande que {}".format(
+    espanya.nombre,
+    "" if espanya.mas_grande_que(francia) else "no ",
+    francia.nombre
+))
 
-print("La densidad de población de {} es {}".format(espanya.nombre, espanya.densidad_poblacion()))
-print("La densidad de población de {} es {}".format(francia.nombre, francia.densidad_poblacion()))
+print("La densidad de población de {} es {}".format(
+    espanya.nombre,
+    espanya.densidad_poblacion()
+))
+
+print("La densidad de población de {} es {}".format(
+    francia.nombre,
+    francia.densidad_poblacion()
+))
