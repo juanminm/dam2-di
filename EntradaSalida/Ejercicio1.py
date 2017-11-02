035993879edd0ae9a4f8970b2fe7715901db2e17
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-11-02 13:42:01 +0100

    Add missing part asked by the exercise

--- EntradaSalida/Ejercicio1.py
+++ EntradaSalida/Ejercicio1.py
@@ -1,7 +1,7 @@
 nombre_fichero = input("Introduzca el nombre del fichero que se copiará: ")
 
 fichero_original = open(nombre_fichero, mode='r', encoding='utf-8')
-fichero_backup = open("bak.txt", mode='w', encoding='utf-8')
+fichero_backup = open(nombre_fichero + "bak.txt", mode='w', encoding='utf-8')
 
 for linea in fichero_original:
     fichero_backup.write(linea)
