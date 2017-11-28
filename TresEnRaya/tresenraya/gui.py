138e364fee8532e6d338204e346e1a6567a714ee
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-11-28 14:06:06 +0100

    Add Cell and rename Grid to GridFrame

--- TresEnRaya/tresenraya/gui.py
+++ TresEnRaya/tresenraya/gui.py
@@ -1,24 +1,43 @@
 import tkinter
 
+
 class MainWindow:
     def __init__(self):
         self.window = tkinter.Tk()
-        Grid(self.window, 3);
+        GridFrame(self.window, 3);
         self.window.mainloop()
 
-class Grid:
+
+class GridFrame:
     def __init__(self, parent, grid_size=3):
         self.parent = parent
+        self.frame = tkinter.Frame()
         self.squares = [[None for i in range(grid_size)] for i in
                          range(grid_size)]
 
+        self.frame.pack()
+
         for i in range(grid_size):
             for j in range(grid_size):
-                self.squares[i][j] = tkinter.Button(
-                    self.parent, textvariable=" ",
-                    command=lambda i=i, j=j: self.choose_square(
-                        self.choose_square(self.squares[i][j])
-                    ),
-                    width=4, height=4
-                )
-                self.squares[i][j].grid(row=i, column=j)
+                self.squares[i][j] = Cell(self.frame, i, j)
+
+
+class Cell:
+    def __init__(self, parent, row, column):
+        self.parent = parent
+        self.row = row
+        self.column = column
+
+        self.button = tkinter.Button(
+            self.parent,
+            textvariable=" ",
+            command=lambda : makeMove(),
+            width=4, height=4
+        )
+
+        self.button.grid(row=self.row, column=self.column)
+
+
+def makeMove():
+    #TODO
+    pass
