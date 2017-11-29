cadd4dbb9369631d72b38b60836182361884c3ff
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-11-29 12:55:13 +0100

    Add frame to set Grid size, finish swapping frames

--- TresEnRaya/tresenraya/gui.py
+++ TresEnRaya/tresenraya/gui.py
@@ -17,16 +17,48 @@ class ModeSelectionFrame:
         self.one_player_button = tkinter.Button(
             self.frame,
             text="One player",
-            # command=change_frame(self.frame, GridSelectionFrame(self.parent))
+            command=lambda: change_frame(self.frame, GridSelectionFrame(self.parent))
         )
         self.one_player_button.pack()
 
         self.two_players_button = tkinter.Button(
             self.frame,
             text="Two players",
-            # command=change_frame(self.frame, GridSelectionFrame(self.parent))
+            command=lambda: change_frame(self.frame, GridSelectionFrame(self.parent))
         )
         self.two_players_button.pack()
+
+
+class GridSelectionFrame:
+    def __init__(self, parent):
+        self.parent = parent
+        self.frame = tkinter.Frame(parent)
+        self.frame.pack(fill="none", expand=True)
+
+        self.input_size_label = tkinter.Label(
+            self.frame,
+            text="Input grid size (empty = 3):"
+        )
+        self.input_size_label.pack()
+
+        self.size_text = tkinter.Text(self.frame)
+        self.size_text.pack()
+
+        self.confirm_grid_size_button = tkinter.Button(
+            self.frame,
+            text="Confirm",
+            command=self.confirm_grid_size
+        )
+        self.confirm_grid_size_button.pack()
+
+    def confirm_grid_size(self):
+        size = self.size_text.get("0.0", tkinter.END)
+
+        if size == '\n':
+            change_frame(self.frame, GridFrame(self.parent))
+        elif isinstance(size, int):
+            change_frame(self.frame, GridFrame(self.parent, size))
+
 
 
 class GridFrame:
@@ -59,9 +91,9 @@ class Cell:
         self.button.grid(row=self.row, column=self.column)
 
 
-def change_frame(frame, param):
-    # TODO
-    pass
+def change_frame(current_frame, next_frame):
+    current_frame.destroy()
+    next_frame
 
 
 def makeMove():
